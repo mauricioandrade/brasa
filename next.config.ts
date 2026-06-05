81cb6e3b44b770aedfee4541f1c528b78ca1a124
mauricioandrade <mauricio_andrade@outlook.com>
2026-06-05 10:55:42 -0300

    feat: allow thesportsdb images in next/image

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.thesportsdb.com',
+        pathname: '/images/**',
+      },
+    ],
+  },
   async headers() {
     return [
       {
@@ -13,6 +22,6 @@ const nextConfig: NextConfig = {
       },
     ]
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
