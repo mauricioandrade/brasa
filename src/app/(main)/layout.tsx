30d309d4bcde594321ef2a040d4209c4ae0ccc4b
mauricioandrade <mauricio_andrade@outlook.com>
2026-06-05 22:04:31 -0300

    feat: wire cookie banner e footer no layout principal

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,16 @@
+import { CookieBanner } from '@/components/layout/cookie-banner'
+import { Footer } from '@/components/layout/footer'
 import { Header } from '@/components/layout/header'
 import { LayoutBackground } from '@/components/layout/layout-background'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-brasa-bg relative">
+    <div className="min-h-screen bg-brasa-bg relative flex flex-col">
       <LayoutBackground />
       <Header />
-      <div className="relative z-10">{children}</div>
+      <div className="relative z-10 flex-1">{children}</div>
+      <Footer />
+      <CookieBanner />
     </div>
   )
 }
